542d8dc39c94a57808803ccb944b90d980b8a98c
fatih <fatihbozkurtankara@gmail.com>
2025-01-21 19:47:04 +0100

    variables

--- class 2/printstatemnt.py
+++ class 2/printstatemnt.py
@@ -8,4 +8,10 @@ y="bozkurt"
 z="huseyin"
 print(f"i am {z} {x} {y}")
 
-print(12+4,"\n", 14%3)
+print(12+4,"\n",14%3)
+
+name="semih"
+age="15"
+dad="fatih"
+mom="berrin"
+print(f"persons name is {name} \n age is {age} \n fathers name: {dad}\n and mothers name: {mom}")
