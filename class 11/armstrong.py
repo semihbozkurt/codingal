1790350f457292fa419e495fdffc13bc97a5000f
fatih <fatihbozkurtankara@gmail.com>
2025-02-20 20:02:02 +0100

    armstrong number

--- class 11/armstrong.py
+++ class 11/armstrong.py
@@ -1,6 +1,18 @@
-num=int(input("write the number"))
+num=int(input("write the number to se if it is armstrong number or not "))
+nmbr=num
 x=0
-while num>0:
-    num=num//10
+while nmbr>0:
+    nmbr=nmbr//10
     x+=1
-print(x)
+
+a=0
+sum=0
+while a<x:
+    sum+=int((str(num)[a]))**x
+
+if sum==num:
+    print("it is a armstrong number")
+else:
+    print("no it is not")
+
+print("something")
