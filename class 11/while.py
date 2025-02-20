c74560067a6295393596bb2feef06ea5d92d9d77
fatih <fatihbozkurtankara@gmail.com>
2025-02-20 19:29:56 +0100

    while loop

---
+++ class 11/while.py
@@ -0,0 +1,22 @@
+i=1
+while i<=5:
+    print("Semih")
+    i=i+1
+
+input("press enter")
+
+b=int(input("write the number"))
+a=1
+while a<=10:
+    print(f"{a} * {b} = {a*b}")
+    a=a+1
+
+input("press enter")
+
+num=int(input("write the number"))
+x=0
+sum=0
+while x<=num:
+    sum+=x
+    x+=1
+print("sum of the numbers is ", sum)
