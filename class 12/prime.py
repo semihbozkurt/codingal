e6bbf64fa49c4f635dbe78b01e9e6a8786f50096
fatih <fatihbozkurtankara@gmail.com>
2025-02-25 20:15:11 +0100

    prime number

--- class 12/prime.py
+++ class 12/prime.py
@@ -1,3 +1,4 @@
+'''
 num1=int(input("write the number "))
 for i in range(2,num1):
     if num1%i==0:
@@ -6,5 +7,16 @@ for i in range(2,num1):
 
     if i==num1-1:
         print("it is a prime number")
-
+'''
         
+lav=(int(input("write the small number ")))
+high=int(input("write the big number "))
+
+for a in range (lav,high+1):
+    for i in range(2,a):
+        if a%i==0:
+            print(a,"is not a prime number",end="\n -------------------- \n")
+            break
+        if i == a-1:
+            print(a,"is a prime number",end="\n -------------------- \n")
+
