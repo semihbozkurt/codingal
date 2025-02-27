8b9b443e29303a4ecbaaeb76feac8dbe0f7c5a7b
fatih <fatihbozkurtankara@gmail.com>
2025-02-27 19:58:04 +0100

    shape fra stars

--- class 13/stars.py
+++ class 13/stars.py
@@ -1,4 +1,24 @@
-n=int(input("write the number of stars "))
 
-for i in range(1,n+1):
-    print("* "*i)
+n=int(input("write a number: "))
+
+count=1
+
+for i in range(n):
+    print("\n")
+    for j in range(i+1):
+        print(count, end=" ")
+        count+=1
+
+
+print(" \n ----------------")
+
+
+num=int(input("write the number of stars "))
+
+clone=num
+
+for i in range(num):
+    for j in range(clone):
+        print("* "*clone)
+        clone-=1
+
