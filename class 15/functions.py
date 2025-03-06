b6e558bba188f9f54191d0c469ce24502bbef2d5
semihbozkurt <semihbozkurtsinop@gmail.com>
2025-03-06 19:24:17 +0100

    calculator with functions

---
+++ class 15/functions.py
@@ -0,0 +1,59 @@
+'''
+def makete():
+    print("boil water")
+    print("put te bag inside")
+    print("wait ")
+    print("take te bag out")
+
+makete()
+'''
+
+
+
+
+
+
+
+
+def add(a,b):
+    print(f"sum of {a} and {b} is {a+b}")
+
+
+def minus(a,b):
+    print(f"{a} minus {b} is {a-b}")
+
+
+def multi(a,b):
+    print(f"{a} * {b} is {a*b}")
+
+def divi(a,b):
+    print(f"{a} divided by {b} is {a/b}")
+
+
+
+choice=str(input('''
+a=adition
+s=substraction
+m=multiplication
+d=division                 
+'''))
+
+
+
+num1=int(input("write the big number"))
+num2=int(input("write the small number"))
+
+
+if choice=="a":
+    add(num1,num2)
+
+elif choice=="s":
+    minus(num1,num2)
+
+elif choice=="m":
+    multi(num1,num2)
+
+elif choice=="d":
+    divi(num1,num2)
+
+print("program ended")
