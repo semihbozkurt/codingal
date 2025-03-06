85a7446aeed6563b6db0b71a2187df78b407b27b
semihbozkurt <semihbozkurtsinop@gmail.com>
2025-03-06 19:51:55 +0100

    circumference of a circle

---
+++ class 15/circumference.py
@@ -0,0 +1,9 @@
+def cf(x,pi):
+    circumference=x*pi
+    print(circumference)
+
+
+diameter=float(input("write the diameter (in the form= xyz.abc)"))
+pi=float(input("what to take pi number as(in form=  xyz.abc)"))
+
+cf(diameter,pi)
