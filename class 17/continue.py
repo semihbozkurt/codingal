391cb3e7307c265f7d2c693864f102c40d43c0d2
semihbozkurt <semihbozkurtsinop@gmail.com>
2025-03-13 19:45:41 +0100

    continue, break and pass

---
+++ class 17/continue.py
@@ -0,0 +1,27 @@
+'''
+
+def f():
+    pass
+for i in range (10):
+    if i==5:
+        continue
+    print(i)
+
+for i in range(5):
+    if i==2:
+        break
+    print(i)
+
+
+'''
+
+
+
+text=input("Write a text ")
+
+for i in text:
+    if i == "A":
+        print("there is A in text")
+        break
+    else: pass
+
