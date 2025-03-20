b7a492bf82509151143c7868c90b82fd1c50bf5c
semihbozkurt <semihbozkurtsinop@gmail.com>
2025-03-20 18:09:17 +0100

    guess game

---
+++ class 19/guess.py
@@ -0,0 +1,23 @@
+import random
+
+rand=random.randint(1,100)
+atempt=0
+
+while True:
+
+    atempt+=1
+
+    try:
+        guess=int(input("write your guess "))
+
+
+        if guess==rand:    
+            print("you win, number of attempts: ",atempt)
+            break
+
+        elif guess < rand: print("too low")
+        elif guess > rand: print("too high")
+
+    except:
+        print("your guess should be integer")
+
