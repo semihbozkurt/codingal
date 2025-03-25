2eefec1a54e9539480945497a629f3f806b8cd3b
semihbozkurt <semihbozkurtsinop@gmail.com>
2025-03-25 18:21:56 +0100

    age calculation

--- class 20/agecalc.py
+++ class 20/agecalc.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import calendar
 now=dt.datetime.now()
 
 day=int(input("write your birth day: "))
@@ -9,3 +10,17 @@ birth=dt.datetime(year,month,day)
 
 age=now-birth
 print("your age is:",age.days//365)
+
+
+
+print("-------------------------------------------------")
+
+weekdays=["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+
+print("today is:",weekdays[now.weekday()])
+
+
+
+print("calendar of december 2025")
+print(calendar.month(2025,12))
+
