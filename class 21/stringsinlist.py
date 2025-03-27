c1046d1be1ffa8d4b081a6dca4403fd0318c1b87
semihbozkurt <semihbozkurtsinop@gmail.com>
2025-03-27 18:27:42 +0100

    number of stings

--- class 21/stringsinlist.py
+++ class 21/stringsinlist.py
@@ -1,5 +1,13 @@
-lst=["abc","aba","123","semih","a","1221"]
+#numbers can also go from the end
+#  no:    -6     -5     -4    -3    -2   -1   
+lst=   ["abc","aba","123","semih","a","1221"]
 
 length=len(lst)
+num=0
 
-print(length)
+for i in range (1,length):
+    if lst[i][0]==lst[i][-1]  and  len(lst[i])>1:
+        num+=1
+    
+
+print(num)
