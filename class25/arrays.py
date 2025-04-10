184334d6198e41352ff002e932d399cbb1e89f06
semihbozkurt <semihbozkurtsinop@gmail.com>
2025-04-10 20:00:51 +0200

    arrays

--- class25/arrays.py
+++ class25/arrays.py
@@ -12,3 +12,10 @@ arr1.append(11)
 arr1.insert(1,16)
 
 print(arr1)
+
+arr2=array.array("i",[1,3,2,3,4,3,5,3,6,3])
+count=0
+for i in range(len(arr2)):
+    if arr2[i]==3:
+        count+=1
+print("number of 3 es in arr2 is: ",count)
