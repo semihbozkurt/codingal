05f7d1a38b9fed716304d591f1a5410e474c9d21
semihbozkurt <semihbozkurtsinop@gmail.com>
2025-05-20 19:26:25 +0200

    pygame

---
+++ class34/game.py
@@ -0,0 +1,20 @@
+import pygame
+pygame.init()
+
+width,height=800,600
+screen=pygame.display.set_mode(width,height)
+
+pygame.display.set_caption("First game")
+
+red=(250,2,2)
+green=(0,250,0)   #rpg numbers
+blue=(0,0,250)
+
+runing=True
+#loop
+while runing:
+    screen.fill(red)
+    pygame.display.update()
+
+pygame.quit()
+
