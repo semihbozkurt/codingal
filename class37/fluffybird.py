bb17e491bc228abde99e3102c3e18ed01a60a100
semihbozkurt <semihbozkurtsinop@gmail.com>
2025-06-17 21:10:59 +0200

    fluffy bird

--- class37/fluffybird.py
+++ class37/fluffybird.py
@@ -4,12 +4,13 @@ pygame.init()
 
 #music
 pygame.mixer.init()
-backsound=pygame.mixer.music.load("myst.mp3.mp3")
+pygame.mixer.music.load("myst.mp3.mp3")
 pygame.mixer.music.play(-1)
 
 collisound=pygame.mixer.Sound("colsn.mp3")
 
 running=True
+gameover=False
 
 hight=600
 width=600
@@ -21,8 +22,9 @@ pygame.display.set_caption("fluffy bird")
 
 score=0
 scorefont=pygame.font.Font(None,32)
+finishfont=pygame.font.Font(None,55)
 
-birdx=1
+birdx=100
 birdy=300
 
 #classes
@@ -30,7 +32,7 @@ class Bird(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image=pygame.Surface((50,50))
-        pygame.draw.circle(self.image,("red"),(50,50),20)
+        pygame.draw.circle(self.image,("red"),(25,25),15)
         
         self.rect = self.image.get_rect()
 
@@ -39,12 +41,12 @@ class Bird(pygame.sprite.Sprite):
         self.rect.y = birdy
 
     def update(self):
-        self.rect.y+=1
+        self.rect.y+=2
 
 class Rectengel(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image=pygame.Surface((100,random.randint(0,500)),pygame.SRCALPHA)
+        self.image=pygame.Surface((100,random.randint(0,500)))
         self.image.fill("green")
 
         self.rect = self.image.get_rect()
@@ -54,7 +56,7 @@ class Rectengel(pygame.sprite.Sprite):
         self.rect.y = 0
 
     def update(self):
-        self.rect.x+=1
+        self.rect.x-=3
 
 #adding the classes
 fluffy=Bird()
@@ -69,18 +71,60 @@ count=0
 while running:
     #screen
     screen.fill("white")
-    textsurfece=scorefont.render(f"score={score}",True,(255,255,255))
+    textsurfece=scorefont.render(f"score={score}",True,(0,0,0))
+    finish=finishfont.render(f"GAME-OVER SCORE:{score}",True,(50,50,100))
     screen.blit(textsurfece,(50,50))
 
     playergroup.draw(screen)
+    playergroup.update()
     enemygroup.draw(screen)
     enemygroup.update()
     
-    count+=1
-    if count%100==0: 
-        ret=Rectengel()
-        enemygroup.add(ret)
+    if fluffy.rect.y>=hight or fluffy.rect.y<=0:
+        fluffy.kill()
+        enemygroup.empty()
+        gameover=True
 
+    if not gameover:
+        if pygame.sprite.spritecollideany(fluffy,enemygroup):
+            fluffy.kill()
+            enemygroup.empty()
+            screen.blit(finish,(1,260))
+            gameover=True
+
+    if gameover:
+        screen.blit(finish,(1,260))
+        pygame.mixer.music.stop()
     
+
+    if not gameover:
+        count+=1
+        if count%140==0: 
+            ret=Rectengel()
+            enemygroup.add(ret)
+
+
+    for ret in enemygroup:
+            if ret.rect.x<=0:
+                ret.kill()
+                score+=1
+    
+    
+
+
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_SPACE:
+                fluffy.rect.y-=60
+            if event.key==pygame.K_r:
+                score=0
+                fluffy=Bird()
+                playergroup.add(fluffy)
+                gameover=False
+                pygame.mixer.music.play(-1)
+
+
     clock.tick(60)
-    pygame.display.update
+    pygame.display.update()
